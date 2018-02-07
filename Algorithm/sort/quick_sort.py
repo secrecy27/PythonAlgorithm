3abe8d2c0363e46df6d74cbeb9c2fa5a41c153c2
secrecy27 <secrecy418@naver.com>
2018-02-07 23:18:08 +0900

    quick_sort

---
+++ Algorithm/sort/quick_sort.py
@@ -0,0 +1,34 @@
+# 퀵정렬 개념
+# 1. 리스트 가운데서 하나의 원소를 고른다. 이를 피벗이라한다.
+# 2. 피벗 앞에는 피벗보다 작은 원소들을 뒤에는 큰 원소들을 오게 한 후
+#    피벗을 기준으로 리스트를 둘로 나눈다. 이렇게 나누는 것을 분할이라 하며 피벗은 더이상 움직이지 않는다.
+# 3. 분할된 두개의 작은 리스트를 recursion으로 이 과정을 반복한다.
+# # 리스트의 크기가 0이나 1이 될 때 까지 반복한다.
+
+
+def quick_sort(a):
+    # 퀵정렬 종료조건
+    if len(a) <= 1:
+        return a
+
+    # 피벗 원소를 고른다.
+    pivot = a[len(a) // 2]
+
+    less = []
+    equal = []
+    more = []
+
+    for i in a:
+        if i < pivot:
+            less.append(i)
+        elif i > pivot:
+            more.append(i)
+        else:
+            equal.append(i)
+
+    return quick_sort(less) + equal + quick_sort(more)
+
+
+a = [5, 6, 3, 4, 2, 1]
+
+print(quick_sort(a))
